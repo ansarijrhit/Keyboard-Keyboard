643e0dfac852af4eb0d6ab0780252a5069ad5a90
ansarijrhit <jordanansari@gmail.com>
2023-12-13 21:21:45 -0800

    Numbers and symbols

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import mido
 import time
 import note_library
 
-inport = mido.open_input()
+def main():
+    inport = mido.open_input()
 
-print("Listening")
-while True:
-    message = inport.receive()
-    if message:
-        if message.type == "note_on":
-            print(note_library.get_single_note(message.note), message.note)
+    print("Listening")
+    while True:
+        message = inport.receive()
+        if message:
+            print(process_input(message))
+
+shifted = False
+letter_mode = True
+def process_input(message):
+    global shifted, letter_mode
+    if message.type == "control_change" and message.control == 72:
+        shifted = not shifted
+    elif message.type == "note_on":
+        if message.note == 45:
+            letter_mode = not letter_mode
+        else:
+            return note_library.get_single_note(letter_mode, shifted, message.note)
+    return ""
+
+main()
--- note_library.py
+++ note_library.py
@@ -1,3 +1,4 @@
+
 single_note_dict = {
     50: "a",
     59: "b",
@@ -8,24 +9,55 @@ single_note_dict = {
     57: "g",
     60: "h",
     64: "i",
+    58: "j",
     46: "k",
     66: "l",
     55: "m",
     62: "n",
     65: "o",
     61: "p",
-    51: "s",
+    56: "q",
     53: "r",
+    51: "s",
     54: "t",
     63: "u",
+    71: "v",
     68: "w",
+    70: "x",
     69: "y",
     49: "z",
     }
-# Missing: J, Q, V, X
 
-def get_single_note(note_num):
-    if note_num in single_note_dict:
-        return single_note_dict[note_num]
+number_dict = {
+    48: "1",
+    50: "2",
+    52: "3",
+    53: "4",
+    55: "5",
+    60: "6",
+    62: "7",
+    64: "8",
+    65: "9",
+    67: "0"
+}
+
+symbol_dict = {
+    48: "!",
+    50: "@",
+    52: "#",
+    53: "$",
+    55: "%",
+    60: "^",
+    62: "&",
+    64: "*",
+    65: "(",
+    67: ")"
+}
+
+def get_single_note(letter_mode, shifted, note_num):
+    if letter_mode and note_num in single_note_dict:
+        return single_note_dict[note_num].capitalize() if shifted else single_note_dict[note_num]
+    elif note_num in number_dict:
+        return symbol_dict[note_num] if shifted else number_dict[note_num]
     return None
 
